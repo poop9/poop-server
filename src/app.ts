d4c783667569f874c9a4b669fcca8b9eaa50b04c
kang-heewon <itphantom@icloud.com>
2019-05-18 20:19:54 +0900

    소켓 추가

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-import { Action, useExpressServer } from 'routing-controllers';
+import socketIo, { Socket } from 'socket.io';
+import { useExpressServer } from 'routing-controllers';
 
 const app: express.Application = express();
+const http = require('http').Server(app);
+const io = socketIo(http);
 
 useExpressServer(app, {
   routePrefix: 'api',
@@ -17,6 +20,12 @@ app.use((err: any, _: express.Request, res: express.Response, ___: express.NextF
   if (!res.headersSent) {
     res.status(err.httpCode || 500).send(err.message || 'something is wrong');
   }
+});
+
+io.on('connection', (socket:Socket) => {
+  socket.on('message', (message: string) => {
+    console.log(message);
+  });
 });
 
 export default app;
