c0139dea52abded68c26124b9f306a30e6d12a9d
kang-heewon <itphantom@icloud.com>
2019-05-18 21:29:24 +0900

    서버 설정 수정

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,8 @@ const io = socketIo(http);
 useExpressServer(app, {
   routePrefix: 'api',
   defaultErrorHandler: false,
-  // api route 연결
-  controllers: [`${__dirname}/controllers/*.[tj]s`],
-  middlewares: [`${__dirname}/middlewares/*.[tj]s`],
-  interceptors: [`${__dirname}/interceptors/*[tj]s`],
 });
 
-// TODO: 모든 에러 response 같은 형태로 보내주도록 추가
 app.use((err: any, _: express.Request, res: express.Response, ___: express.NextFunction) => {
   if (!res.headersSent) {
     res.status(err.httpCode || 500).send(err.message || 'something is wrong');
